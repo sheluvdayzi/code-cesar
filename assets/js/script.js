a57597cd4c75da8b68aef914f20d4a08969e68a7
sheluvdayzi <ziyadclombe.pro@gmail.com>
2023-04-17 10:01:15 +0200

    en cours7

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
-let phrase = prompt("Ecris moi ta phrase");
+function cesar(text, nb){
+    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'y', 'z'];
+    let newTab = [];
+    let result = [];
+    newTab = text.split('');
+    console.log(newTab);
+    for(let i = 0; i < newTab.length; i++){
+        if(newTab = alphabet){
+            result = newTab + nb;
+        }
+        return result;
+    }
+}
+
+console.log(cesar('bonjour je suis ziyad', 2));
