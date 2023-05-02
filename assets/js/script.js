9800ffdded835b8b086ac572f74b379eb8dbf6c2
sheluvdayzi <ziyadclombe.pro@gmail.com>
2023-05-02 17:30:10 +0200

    code cesar en cours

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,35 @@
-function cesar(text, nb){
-    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'y', 'z'];
-    let newTab = [];
+function cesar(text , nb){
+    const alpha = [',', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'y', 'z']; // L'alphabet
+    let arrSplit = []; // Stocker le text split
     let result = [];
-    newTab = text.split('');
-    console.log(newTab);
-    for(let i = 0; i < newTab.length; i++){
-        if(newTab = alphabet){
-            result = newTab + nb;
-        }
-        return result;
+    let finalres = [];
+    let index = [];
+    arrSplit = text.split('');
+    console.log(arrSplit);
+    for(let i = 0; i < arrSplit.length; i++){
+        index = alpha.indexOf(arrSplit[i]);
+        result.push(index + nb);
+        console.log(index);
     }
+    for(let i = 0; i < alpha.length; i++){
+        if(index[i] == alpha.length - 1){
+            finalres.push(alpha[0 + 1]);
+            console.log('abc');
+        }
+        if(alpha[result[i]] !== undefined){
+            if(alpha.indexOf(result[i]) == 0){
+                finalres.push(',');
+            } 
+            else if(result[i] == ' '){
+                finalres.push(' ');
+            } 
+            else{
+                finalres.push(alpha[result[i]]);    
+            }
+        }
+
+    }
+    return finalres.join('');
 }
 
-console.log(cesar('bonjour je suis ziyad', 2));
+console.log(cesar('bonjour, je suis ziyad', 1)); 
